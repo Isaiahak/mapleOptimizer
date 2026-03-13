bdba38d75fabc6f4ca20e6f939045f51876ffe71
isaiah <isaiahkenny321123@gmail.com>
2026-03-12 23:47:37 -0400

    need to fix the histogram functions

--- utils/pixelDetector.go
+++ utils/pixelDetector.go
@@ -16,6 +16,20 @@ import (
 	"strings"
 	"unsafe"
 )
+
+type Axis int
+
+const (
+	Vertical = iota
+	Horizontal
+)
+
+var xAxis Axis = Vertical
+var yAxis Axis = Horizontal
+
+var skillOffset1920x1080 = 49
+var skillOffset1366x768 = 35
+
 var blackPxl = struct {
 	r uint8
 	g uint8
@@ -23,42 +37,40 @@ var blackPxl = struct {
 	a uint8
 }{
 	r: 7,
-	g:15,
-	b:33,
-	a:255,
+	g: 15,
+	b: 33,
+	a: 255,
 }
 
-type Rect = struct{
+type Rect = struct {
 	xStart int64
-	xEnd int64
+	xEnd   int64
 	yStart int64
-	yEnd int64
-	width int64
+	yEnd   int64
+	width  int64
 	height int64
-	size int64
+	size   int64
 }
 
 var skillBar1920x1080 = Rect{
 	xStart: 1100,
-	xEnd: 1920,
+	xEnd:   1920,
 	yStart: 950,
-	yEnd: 1080,
-	width: 720,
+	yEnd:   1080,
+	width:  720,
 	height: 130,
-	size: 720 * 130, 
-
+	size:   720 * 130,
 }
 
 var skillBar1366x768 = Rect{
 	xStart: 500,
-	xEnd: 920,
+	xEnd:   920,
 	yStart: 450,
-	yEnd: 520,
-	width: 420,
+	yEnd:   520,
+	width:  420,
 	height: 70,
-	size: 420 * 70, 
+	size:   420 * 70,
 }
-
 
 /*
 for center
@@ -208,7 +220,7 @@ func LoadVideo() {
 		panic(err)
 	}
 
-	fmt.Println("screen resolution : width: ",width,", height: ", height)
+	fmt.Println("screen resolution : width: ", width, ", height: ", height)
 	// for actual algorithm
 	//cmd := exec.Command("ffmpeg", "-i", videoPath, "-vf", "fps=1", "-f", "rawvideo", "-pix_fmt", "rgb24", "-")
 
@@ -227,18 +239,21 @@ func LoadVideo() {
 	var buf = make([]byte, frameSize)
 
 	var skillBar Rect
+	var skillOffset int
 	switch {
-		case width == 1920  && height == 1080:  
-			skillBar = skillBar1920x1080
-		case width == 1366 && height == 768: 
-			skillBar = skillBar1366x768
-		default:
-			return 
-	}	
-	
-	fmt.Println("skill bar resolution : width: ", skillBar.width,", height: ", skillBar.height)
+	case width == 1920 && height == 1080:
+		skillBar = skillBar1920x1080
+		skillOffset = skillOffset1920x1080
+	case width == 1366 && height == 768:
+		skillBar = skillBar1366x768
+		skillOffset = skillOffset1366x768
+	default:
+		return
+	}
 
-	var skills =  make([]byte, skillBar.size) 
+	fmt.Println("skill bar resolution : width: ", skillBar.width, ", height: ", skillBar.height)
+
+	var skills = make([]byte, skillBar.size)
 
 	rl.InitWindow(int32(skillBar.width), int32(skillBar.height), "vod")
 	rl.SetTargetFPS(5)
@@ -247,7 +262,6 @@ func LoadVideo() {
 	img := rl.GenImageColor(int(skillBar.width), int(skillBar.height), rl.Black)
 	texture := rl.LoadTextureFromImage(img)
 	rl.UnloadImage(img)
-
 	for !rl.WindowShouldClose() {
 		_, err = io.ReadFull(stdout, buf)
 		if err != nil {
@@ -257,20 +271,44 @@ func LoadVideo() {
 				log.Fatal(err)
 			}
 		}
-		
+
 		skills = skills[:0]
 		rowStride := width * 4
-		for y := skillBar.yStart; y < skillBar.yStart + skillBar.height; y++{
-			start := y * rowStride + skillBar.xStart * 4
-			end := start + skillBar.width * 4
+		for y := skillBar.yStart; y < skillBar.yStart+skillBar.height; y++ {
+			start := y*rowStride + skillBar.xStart*4
+			end := start + skillBar.width*4
 			skills = append(skills, buf[start:end]...)
 		}
 
-		checkHistogram(skills, skillBar)
-		
-		rgba := unsafe.Slice((*color.RGBA)(unsafe.Pointer(&skills[0])), skillBar.height*skillBar.width) 
+		verticalOffset := getOffset(skills, skillBar, xAxis)
+		horizontalOffset := getOffset(skills, skillBar, yAxis)
+
+		fmt.Println("vertical offset starts at: ", verticalOffset, "\nhorizontal offsey starts at: ", horizontalOffset)
+
+		rgba := unsafe.Slice((*color.RGBA)(unsafe.Pointer(&skills[0])), skillBar.height*skillBar.width)
+
+		//draw vertical offset
+		for i := 0; i < int(skillBar.height); i++ {
+			for col := horizontalOffset; col < int(skillBar.width); col += skillOffset {
+				idx := i*int(skillBar.width) + col
+				rgba[idx] = color.RGBA{255, 255, 255, 255}
+			}
+		}
+
+		//draw horizontal offset
+		var pos int
+		for j := 0; j < int(skillBar.width); j++ {
+			pos = j + int(skillBar.width)*verticalOffset
+			rgba[pos] = color.RGBA{255, 255, 255, 255}
+		}
+
 		rl.UpdateTexture(texture, rgba)
-	
+
+		/*
+			takeScreenshot(texture, "resources/skillbar_snapshot_"+strconv.FormatInt(num, 10)+".png")
+			num++
+		*/
+
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.Black)
 		rl.DrawTexture(texture, 0, 0, rl.White)
@@ -278,61 +316,121 @@ func LoadVideo() {
 	}
 
 	if err := cmd.Wait(); err != nil {
-		log.Fatal(err)
+		fmt.Println("end of video")
 	}
 
-	
 }
 
-
-func checkRowForBlack(buf []byte) bool{
+func checkRowForBlack(buf []byte) bool {
 	totalPixels := len(buf) / 4
-	blackPixels := 0 
-	for i := 0; i < totalPixels; i+= 4{
-		r,g,b := buf[i], buf[i+1], buf[i+2]
-		if r <= blackPxl.r && g <= blackPxl.g && b <= blackPxl.b{
+	blackPixels := 0
+	for i := 0; i < totalPixels; i += 4 {
+		r, g, b := buf[i], buf[i+1], buf[i+2]
+		if r <= blackPxl.r && g <= blackPxl.g && b <= blackPxl.b {
 			blackPixels++
 		}
 	}
-	if blackPixels != totalPixels && float32(blackPixels) > float32(totalPixels) * 0.2  {
+	if blackPixels != totalPixels && float32(blackPixels) > float32(totalPixels)*0.2 {
 		return true
 	}
 	return false
 }
 
-func checkHistogram(buf []byte, skillBar Rect) {
-	var histogram = make([]float32, skillBar.width)
-
-	var i int64 = 0
-	var index int64 = 0 
-	for ; i < skillBar.size; i += 4{
-		histogram[index] += float32(buf[i]) + float32(buf[i+1]) + float32(buf[i+2])
-		//histogram[index] += float32(buf[i])		
-		//histogram[index] += float32(buf[i+1]) 	
-		//histogram[index] += float32(buf[i+2])
-
-		if index == skillBar.width - 1 {
-			index = 0
-		} else {
-			index++
+func getOffset(buf []byte, skillBar Rect, axis Axis) int {
+	histogram := checkHistogram(buf, skillBar, axis)
+	correlationResult := checkCorrelation(histogram, skillBar, axis)
+	minIndex := 0
+	minValue := correlationResult[0]
+	for i, v := range correlationResult {
+		if v < minValue {
+			minValue = v
+			minIndex = i
 		}
 	}
-	
-	var buffer = make([]float32, 3)
-	for i := 0; i < len(histogram); i++ {
-		switch i {
-			case i < len(histogram) - 2:
+	return minIndex
 
-		}
-			buffer[i] buffer[i+1] buffer[i+2] 	
-			fmt.Println("col: ",col,", color intensity: ", intensity)
-		}
-	}
 }
 
-// texture used to create an img and filepath should be a complete path to the desired file 
-func takeScreenshot(texture rl.Texture2D, filePath string){
+func checkCorrelation(histogram []float32, skillBar Rect, axis Axis) []float32 {
+	var offset int
+	var size int
+	switch axis {
+	case Vertical:
+		offset = 50
+		size = int(skillBar.width)
+
+	case Horizontal:
+		offset = 50
+		size = int(skillBar.height)
+	}
+
+	var correlationResult = make([]float32, offset)
+	i := 1
+	var width = 2
+	for ; i < offset; i++ {
+		var correlationSum float32 = 0
+		for pos := 0; pos < size-i-width; pos++ {
+			colSum1 := histogram[pos] * histogram[pos+i]
+			colSum2 := histogram[pos+1] * histogram[pos+i+1]
+			colSum3 := histogram[pos+width] * histogram[pos+i+width]
+			correlationSum += (colSum1 + colSum2 + colSum3) / 3
+
+		}
+		correlationResult[i-1] = correlationSum
+	}
+	return correlationResult
+}
+
+func checkHistogram(buf []byte, skillBar Rect, axis Axis) []float32 {
+	var size int64
+	switch axis {
+	case Vertical:
+		size = skillBar.width
+	case Horizontal:
+		size = skillBar.height
+	}
+	var histogram = make([]float32, size)
+
+	var i int64 = 0
+	var index int64 = 0
+
+	switch axis {
+	case Vertical:
+		for ; i < size; i++ {
+			var j int64 = 0
+			for ; j < skillBar.width; j += 4 {
+				rowStride := skillBar.width*4 + i
+				histogram[i] += float32(buf[rowStride+j] + buf[rowStride+j+1] + buf[rowStride+j+2])
+				//histogram[i] += float32(buf[rowStride + j])
+				//histogram[i] += float32(buf[rowStride+j+1])
+				//histogram[i] += float32(buf[rowStride+j+2])
+
+			}
+		}
+
+	case Horizontal:
+		for ; i < size; i++ {
+			var j int64 = 0
+			for ; j < skillBar.width*4; j += 4 {
+				histogram[i] += float32(buf[j] + buf[j+1] + buf[j+2])
+				//histogram[i] += float32(buf[i])
+				//histogram[i] += float32(buf[i+1])
+				//histogram[i] += float32(buf[i+2])
+
+				if index == skillBar.width-1 {
+					index = 0
+				} else {
+					index++
+				}
+			}
+		}
+	}
+	return histogram
+}
+
+// texture used to create an img and filepath should be a complete path to the desired file
+func takeScreenshot(texture rl.Texture2D, filePath string) {
 	img := rl.LoadImageFromTexture(texture)
-	rl.ExportImage(*img, filePath)	
+	rl.ExportImage(*img, filePath)
 
 }
